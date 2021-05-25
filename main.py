2002b54e409b9b1ed1e0783072a8b1f16271258f
Prof. Diogo S. Martins <santana.martins@ufabc.edu.br>
2021-05-25 16:49:31 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Print a simple "hello world" message
+@author: diogo
+"""
+
+def say_hello():
+    return "Hello World."
+
+if __name__ == "__main__":
+    print(say_hello())
